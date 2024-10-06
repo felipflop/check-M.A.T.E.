0cf4679008c2bc121b870c9bd18d0550c1ecb5bc
felipflop <felipe.canazil@gmail.com>
2024-10-06 14:46:34 -0700

    feat: client encapsulation

--- src/checkmate.py
+++ src/checkmate.py
@@ -1,32 +1,21 @@
-import berserk
+from lichess_api_client import LichessClient
 import time
-
-# client = berserk.Client()
 
 class CheckMATE:
     def __init__(self, api_token_path):
-        with open(api_token_path, 'r') as file:  
-            self.token = file.read()
+        self.client = LichessClient(api_token_path)
 
-    def client_connect(self):
-        global client
-        session = berserk.TokenSession(self.token)
-        client = berserk.Client(session)
-        # print("[S] Client connected")
+    def begin(self):
+        self.client.connect()
 
-    def get_info(self):
-        print(client.account.get())
-    
     def menu(self):
-        self.client_connect()
-
         while True:
             option = input("Enter an option:\n1: Get account information\n2: Start Game\n3: Exit\n")
 
             if option == '1':
-                print(client.account.get())
+                self.client.get_info()
             elif option == '2':
-                print(client.account.get())
+                print("Starting game ...")
             elif option == '3':
                 break
             else:
@@ -34,6 +23,6 @@ class CheckMATE:
 
             time.sleep(1)
 
-        
+
 
         
