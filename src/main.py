10388d363cc455e80328bcc43c0ee3cd3b2e5f7a
felipflop <felipe.canazil@gmail.com>
2024-10-06 14:42:11 -0700

    [fix]: encapsulation

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ from checkmate import CheckMATE
 # Replace with absolute path to your own API token"
 api_token_path = "/home/flop/api-token.txt"
 
-board = CheckMATE(api_token_path)
+checkmate = CheckMATE(api_token_path)
 
 def main():
-    board.menu()
+    checkmate.begin()
+    checkmate.menu()
 
 if __name__ == '__main__':
     main()
