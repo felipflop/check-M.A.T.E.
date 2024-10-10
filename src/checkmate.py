7f9bbe03ab2cce6f4ab7db96495fd93deb68813f
felipflop <felipe.canazil@gmail.com>
2024-10-10 10:51:44 -0700

    feat: simple ASCII gameplay

--- src/checkmate.py
+++ src/checkmate.py
@@ -1,46 +1,57 @@
+import time
+import os
 from lichess_api_client import LichessClient
 from chess_game import ChessGame
-import time
+
 
 class CheckMATE:
     def __init__(self, api_token_path):
         self.client = LichessClient(api_token_path)
+        self.game = ChessGame()
 
     def begin(self):
         self.client.connect()
 
-    def play_player(self):
-        # self.game = ChessGame()
-        # self.game.print_board()
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    def refresh_board(self):
+        self.clear_screen()
+        self.game.print_board()
 
-        # """ call client to request game -> return colour """
-        # # colour = client.start_game()
-        # colour = "black"
+    def play_player(self):  
+        self.refresh_board()
+
+        while True:
+            move = input("Enter move or q to quit: ")
+            if move == 'q':
+                break
+
+            else:
+                self.game.move_piece(move)
+                self.refresh_board()
     
-        # if colour is "black":
-        #     # move = client.get_first_move()
-        #     move = "e2e4"
-        #     # self.game.board.push_san(move)
-        #     self.game.move_piece(move)
-        #     self.game.print_board()
-    
-    def play_bot():
+        self.clear_screen()
+
+    def play_robot():
         return 
 
     def menu(self):
         while True:
-            option = input("Enter an option:\n1: Start Game\n2: Account Information\n3: Exit\n")
+            option = input("Enter an option:\n1: Play online\n2: Play robot\n3: Account Information\n4: Exit\n")
 
             if option == '1':
-                self.run()
+                self.play_player()
             elif option == '2':
-                self.client.get_info()
+                self.play_robot()
             elif option == '3':
+                self.client.get_info()
+            elif option == '4':
                 break
             else:
                 print("Invalid!")
 
-            time.sleep(1)
+            
 
 
 
